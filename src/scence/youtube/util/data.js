1c5e43bc67ea38b5ebceba34e17dd735313fe726
Mrshinezee <ijeomaharinze@gmail.com>
2019-07-20 22:58:23 +0100

    design of youtube landing page

---
+++ src/scence/youtube/util/data.js
@@ -0,0 +1,32 @@
+const data = [
+  {
+    url:
+      'https://d2qp0siotla746.cloudfront.net/img/landing-pages/youtube-thumb/landing-page-ytt-3.png',
+    channel: 'https://randomuser.me/api/portraits/women/6.jpg',
+    title: ' Earn Revenue from your local content',
+    views: '55,788 views',
+  },
+  {
+    url:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx_gNn9_dJKTS9kZKQSjSvdBT-9KL2xZgBvjb7RBe_ik7xY4_X',
+    channel: 'https://randomuser.me/api/portraits/women/48.jpg',
+    title: ' Earn Revenue from your local content',
+    views: '85,788 views',
+  },
+  {
+    url:
+      'https://d2qp0siotla746.cloudfront.net/img/landing-pages/youtube-thumb/landing-page-ytt-3.png',
+    channel: 'https://randomuser.me/api/portraits/women/6.jpg',
+    title: ' Earn Revenue from your local content',
+    views: '55,788 views',
+  },
+  {
+    url:
+      'https://d2qp0siotla746.cloudfront.net/img/landing-pages/youtube-thumb/landing-page-ytt-3.png',
+    channel: 'https://randomuser.me/api/portraits/women/6.jpg',
+    title: ' Earn Revenue from your local content',
+    views: '55,788 views',
+  },
+];
+
+export default data;
